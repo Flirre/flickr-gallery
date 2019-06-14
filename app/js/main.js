d7dc203651bbe2252ba5787197ddb54ce4bcdc59
Filip Lindahl <gysztaf@gmail.com>
2019-06-14 13:23:24 +0200

    fetch photos

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,24 @@
 // Your scripts goes here...
 
 document.addEventListener("DOMContentLoaded", function() {
-  console.log("Taking off... 🚀");
-  let images = [];
   fetch(
-    "https://www.flickr.com/services/rest/?method=flickr.photos.search&format=json&nojsoncallback=1&api_key=0d8c0f6a218bd0f4408bda4965712e2e&tags=cat&safe_search=1&content_type=1&media=photos&per_page=15&page=2;"
+    "https://www.flickr.com/services/rest/?method=flickr.photos.search&format=json&nojsoncallback=1&api_key=0d8c0f6a218bd0f4408bda4965712e2e&tags=cat&safe_search=1&content_type=1&media=photos&extras=url_o&per_page=15&page=2;"
   )
     .then(function(response) {
       return response.json();
     })
     .then(function(json) {
-      console.log(JSON.stringify(json));
+      jsonPhotos = json.photos.photo;
+      for (let i = 0; i < jsonPhotos.length; i++) {
+        if (jsonPhotos[i] && jsonPhotos[i].url_o) {
+          appendPhoto(jsonPhotos[i].url_o);
+        }
+      }
     });
 });
+
+function appendPhoto(url) {
+  const imageElem = document.createElement("img");
+  imageElem.setAttribute("src", url);
+  document.getElementById("gallery").appendChild(imageElem);
+}
